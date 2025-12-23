fc7b59dae451d4f8f068575aba3f9b4b92011642
Soel Antonio Attala <soelattala@outlook.com>
2025-12-23 15:04:57 -0300

    adding starting files and config

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          50: '#f8faf6',
+          100: '#f0f5eb',
+          200: '#e1ebd7',
+          300: '#c8dbb8',
+          400: '#a8d5ba',
+          500: '#6b9f4d',
+          600: '#4a7c2c',
+          700: '#2d5016',
+          800: '#1e3610',
+          900: '#0f1b08',
+        },
+      },
+      fontFamily: {
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif',
+        ],
+      },
+    },
+  },
+  plugins: [],
+}
